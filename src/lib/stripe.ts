f52a0b47efd5b3fcdd74376a08722c7bfb0146ae
Dimitry <d.abramiani@gmail.com>
2026-02-11 02:10:27 +0400

    Fix Stripe API version to 2026-01-28.clover

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,7 +6,7 @@ let stripe: Stripe | null = null;
 
 if (process.env.STRIPE_SECRET_KEY) {
   stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-12-15.clover",
+    apiVersion: "2026-01-28.clover",
     typescript: true,
   });
 }
